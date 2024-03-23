6a04d2be761b9342ae6c58f3f46d2d80542fd6c2
B I <bimohammadabbas@gmail.com>
2024-03-23 13:37:25 +0530

    New models and table structure

--- rx-assist/core/migrations/0001_initial.py
+++ rx-assist/core/migrations/0001_initial.py
@@ -47,22 +47,6 @@ class Migration(migrations.Migration):
             ],
         ),
         migrations.CreateModel(
-            name='Medical',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('s1', models.CharField(max_length=200)),
-                ('s2', models.CharField(max_length=200)),
-                ('s3', models.CharField(max_length=200)),
-                ('s4', models.CharField(max_length=200)),
-                ('s5', models.CharField(max_length=200)),
-                ('disease', models.CharField(max_length=200)),
-                ('medicine', models.CharField(max_length=200)),
-                ('created_on', models.DateTimeField(auto_now_add=True)),
-                ('doctor', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='doctor', to=settings.AUTH_USER_MODEL)),
-                ('patient', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='patient', to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
-        migrations.CreateModel(
             name='Profile',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
@@ -72,19 +56,6 @@ class Migration(migrations.Migration):
                 ('gender', models.CharField(max_length=255)),
                 ('country', models.CharField(default='Tanzania', max_length=255)),
                 ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='Ment',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('approved', models.BooleanField(default=False)),
-                ('time', models.CharField(max_length=200, null=True)),
-                ('ment_day', models.DateTimeField(null=True)),
-                ('created_on', models.DateTimeField(auto_now_add=True)),
-                ('doctor', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='dor', to=settings.AUTH_USER_MODEL)),
-                ('medical', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='medical', to='core.medical')),
-                ('patient', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='pat', to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
--- rx-assist/core/models.py
+++ rx-assist/core/models.py
@@ -10,40 +10,6 @@ class User(AbstractUser):
     phonenumber = models.CharField(max_length=200, null=True)
 
 
-class Medical(models.Model):
-    s1 = models.CharField(max_length=200)
-    s2 = models.CharField(max_length=200)
-    s3 = models.CharField(max_length=200)
-    s4 = models.CharField(max_length=200)
-    s5 = models.CharField(max_length=200)
-    disease = models.CharField(max_length=200)
-    medicine = models.CharField(max_length=200)
-    patient = models.ForeignKey(
-        User, related_name="patient", on_delete=models.CASCADE)
-    doctor = models.ForeignKey(
-        User, related_name="doctor", on_delete=models.CASCADE, null=True)
-    created_on = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.disease
-
-
-class Ment(models.Model):
-    approved = models.BooleanField(default=False)
-    time = models.CharField(max_length=200, null=True)
-    patient = models.ForeignKey(
-        User, related_name="pat", on_delete=models.CASCADE)
-    doctor = models.ForeignKey(
-        User, related_name="dor", on_delete=models.CASCADE, null=True)
-    ment_day = models.DateTimeField(null=True)
-    medical = models.ForeignKey(
-        Medical, related_name="medical", on_delete=models.CASCADE, null=True)
-    created_on = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.approved
-
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(
