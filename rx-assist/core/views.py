36b085467621a05ced5d311dc6db56d9659309af
B I <bimohammadabbas@gmail.com>
2024-03-24 12:52:34 +0530

    Integration completed after saving symptoms

--- rx-assist/core/views.py
+++ rx-assist/core/views.py
@@ -137,26 +137,29 @@ def MakePredict(request):
     id = request.POST.get('id')
     symptoms = request.POST.get('symptoms')
     list_b = list(set(symptoms.split(',')))
-    print(list_b)
     list_a = ['itching', 'skin_rash', 'nodal_skin_eruptions', 'continuous_sneezing', 'shivering', 'chills', 'joint_pain', 'stomach_pain', 'acidity', 'ulcers_on_tongue', 'muscle_wasting', 'vomiting', 'burning_micturition', 'spotting_ urination', 'fatigue', 'weight_gain', 'anxiety', 'cold_hands_and_feets', 'mood_swings', 'weight_loss', 'restlessness', 'lethargy', 'patches_in_throat', 'irregular_sugar_level', 'cough', 'high_fever', 'sunken_eyes', 'breathlessness', 'sweating', 'dehydration', 'indigestion', 'headache', 'yellowish_skin', 'dark_urine', 'nausea', 'loss_of_appetite', 'pain_behind_the_eyes', 'back_pain', 'constipation', 'abdominal_pain', 'diarrhoea', 'mild_fever', 'yellow_urine', 'yellowing_of_eyes', 'acute_liver_failure', 'fluid_overload', 'swelling_of_stomach', 'swelled_lymph_nodes', 'malaise', 'blurred_and_distorted_vision', 'phlegm', 'throat_irritation', 'redness_of_eyes', 'sinus_pressure', 'runny_nose', 'congestion', 'chest_pain', 'weakness_in_limbs', 'fast_heart_rate', 'pain_during_bowel_movements', 'pain_in_anal_region', 'bloody_stool', 'irritation_in_anus', 'neck_pain', 'dizziness', 'cramps', 'bruising', 'obesity', 'swollen_legs', 'swollen_blood_vessels', 'puffy_face_and_eyes', 'enlarged_thyroid',
               'brittle_nails', 'swollen_extremeties', 'excessive_hunger', 'extra_marital_contacts', 'drying_and_tingling_lips', 'slurred_speech', 'knee_pain', 'hip_joint_pain', 'muscle_weakness', 'stiff_neck', 'swelling_joints', 'movement_stiffness', 'spinning_movements', 'loss_of_balance', 'unsteadiness', 'weakness_of_one_body_side', 'loss_of_smell', 'bladder_discomfort', 'foul_smell_of urine', 'continuous_feel_of_urine', 'passage_of_gases', 'internal_itching', 'toxic_look_(typhos)', 'depression', 'irritability', 'muscle_pain', 'altered_sensorium', 'red_spots_over_body', 'belly_pain', 'abnormal_menstruation', 'dischromic _patches', 'watering_from_eyes', 'increased_appetite', 'polyuria', 'family_history', 'mucoid_sputum', 'rusty_sputum', 'lack_of_concentration', 'visual_disturbances', 'receiving_blood_transfusion', 'receiving_unsterile_injections', 'coma', 'stomach_bleeding', 'distention_of_abdomen', 'history_of_alcohol_consumption', 'fluid_overload', 'blood_in_sputum', 'prominent_veins_on_calf', 'palpitations', 'painful_walking', 'pus_filled_pimples', 'blackheads', 'scurring', 'skin_peeling', 'silver_like_dusting', 'small_dents_in_nails', 'inflammatory_nails', 'blister', 'red_sore_around_nose', 'yellow_crust_ooze']
     list_c = []
     for x in range(0, len(list_a)):
         list_c.append(0)
     Symptoms=0
-    for z in range(0, len(list_a)):
-        for inputSymptoms in list_b:
-            if (inputSymptoms == list_a[z]):
-                list_c[z] = 1
-                Symptoms=Symptoms+1
+    i = 0
+    while i < len(list_b):
+        inputSymptoms = list_b[i]
+        if inputSymptoms in list_a:
+            index = list_a.index(inputSymptoms)
+            list_c[index] = 1
+            Symptoms += 1
+            i += 1
+        else:
+            del list_b[i]
     print("No of Symptoms: ",Symptoms)
+    print(list_b)
     if Symptoms == 0:
         return JsonResponse({'status': 'error'})
     test = list_c
     test = np.array(test)
     test = np.array(test).reshape(1, -1)
-    print(test.shape)
-
     modelDT = joblib.load('model/decision_tree.pkl')
     modelNB = joblib.load('model/naive_bayes.pkl')
     modelLP = joblib.load('model/lopistic_regression.pkl')
@@ -184,8 +187,10 @@ def MakePredict(request):
         result = f"4: {result_candidates}"
     c = connection.cursor()
     c.execute("INSERT INTO patient_diagnosis (disease, patient_id, medicine, created_on, num_symptoms) VALUES (%s, %s, '', %s, %s)", (result, id, datetime.now(), Symptoms))
-
-
+    diagnosis_id = c.lastrowid
+    print("diagnosis Id",diagnosis_id)
+    for symptom in list_b:
+        c.execute("INSERT INTO diagnosis_symptoms (patient_diagnosis_id, symptom) VALUES (%s, %s)", (diagnosis_id, symptom))
     return JsonResponse({'status': message})
 
 @login_required
